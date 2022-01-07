f45115fbf2b890fd118348f9eb03ae7f6798b80d
Tabby Douglass <tabbytoney@gmail.com>
2022-01-06 18:02:16 -0600

    Setup for dynamic data in ExpenseData

--- expense-tracker-react/src/components/ExpenseItem.js
+++ expense-tracker-react/src/components/ExpenseItem.js
@@ -2,12 +2,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  // months start at 0, so March is month 2
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <div className="expense-item">
-      <div>March 28, 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    // use .toISOString to get the date to show as a string. Otherwise it'll break
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
