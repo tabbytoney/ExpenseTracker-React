2ae46ae4ffea8b7ca7b2449d1461a0bc30738a7e
Tabby Douglass <tabbytoney@gmail.com>
2022-01-06 18:24:42 -0600

    Added props and expense items to app.js

--- expense-tracker-react/src/components/ExpenseItem.js
+++ expense-tracker-react/src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
 // For rendering expense item data
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   // months start at 0, so March is month 2
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
 
   return (
     // use .toISOString to get the date to show as a string. Otherwise it'll break
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
