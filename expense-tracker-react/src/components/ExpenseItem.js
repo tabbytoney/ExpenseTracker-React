919494a6b49dd832349ac591587fa0fd6ea1ab0d
Tabby Douglass <tabbytoney@gmail.com>
2022-01-07 16:53:42 -0600

    Added date formatting to ExpenseItem via toLocaleString

--- expense-tracker-react/src/components/ExpenseItem.js
+++ expense-tracker-react/src/components/ExpenseItem.js
@@ -3,11 +3,19 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   // months start at 0, so March is month 2
+  // .toLocaleString gives you options of how to display the date
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
 
   return (
-    // use .toISOString to get the date to show as a string. Otherwise it'll break
+    // use props.date.toISOString to get the date to show as a string. Otherwise it'll break. Not pretty though
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
