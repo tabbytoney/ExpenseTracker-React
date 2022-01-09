2050b46826038dfe742657293f09c6ebea357627
Tabby Douglass <tabbytoney@gmail.com>
2022-01-09 14:13:29 -0600

    Added variable for title to change,  no functionality yet

--- expense-tracker-react/src/components/Expenses/ExpenseItem.js
+++ expense-tracker-react/src/components/Expenses/ExpenseItem.js
@@ -7,14 +7,17 @@ const ExpenseItem = (props) => {
   // Card is a custom component - with props.children, everything inside the <Card> tags
   // is available to the Card component in Card.js
 
+  // props gets the current value of the title
+  let title = props.title
+
   const clickHandler = () => {
-    console.log('Clicked it!');
+    title = 'Updated'
   };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
