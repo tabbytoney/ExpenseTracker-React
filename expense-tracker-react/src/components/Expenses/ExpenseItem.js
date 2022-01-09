4b45d911ae9dbc90a963208dda92b52c7f68045a
Tabby Douglass <tabbytoney@gmail.com>
2022-01-09 14:57:25 -0600

    Add useState to update title

--- expense-tracker-react/src/components/Expenses/ExpenseItem.js
+++ expense-tracker-react/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 // For rendering expense item data
+import React, { useState } from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -7,13 +8,16 @@ const ExpenseItem = (props) => {
   // Card is a custom component - with props.children, everything inside the <Card> tags
   // is available to the Card component in Card.js
 
+  // React hooks like useState should only be called directly inside a component function, not outside or in nested functions
   // props gets the current value of the title
-  let title = props.title
+   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = 'Updated'
+    setTitle('Updated!'); // function called to change the state of title 
+    console.log(title)
   };
-  return (
+  
+  return ( 
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
