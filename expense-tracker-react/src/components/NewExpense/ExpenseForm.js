31495cc6b8ea7155462a2061466d9a83431a8d86
Tabby Douglass <tabbytoney@gmail.com>
2022-01-12 17:50:00 -0600

    When state depends on a previous state, returned to multiple states

--- expense-tracker-react/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker-react/src/components/NewExpense/ExpenseForm.js
@@ -3,45 +3,52 @@ import { useState } from 'react';
 
 const ExpenseForm = () => {
   // using multiple states
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   // using only one state by passing in an object
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
 
   const titleChangeHandler = (event) => {
     // single state usage
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     // prints the value - aka what's typed - into the title text box
     // console.log(event.target.value)
 
-    setUserInput({
-      // ... pulls out all the key value pairs and returns the values. Title is overridden because it's specifically mentioned here
-      // If you dont do this and just have enteredTitle, enteredAmount and enteredDate just get dropped
-      ...userInput,
-      enteredTitle: event.target.value
-    });
+    // not best way to call setUserInput, esp if doing a counter or something that depends on a previous state
+    // setUserInput({
+    //   // ... pulls out all the key value pairs and returns the values. Title is overridden because it's specifically mentioned here
+    //   // If you dont do this and just have enteredTitle, enteredAmount and enteredDate just get dropped
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // });
+
+    // passing a function to setUserInput and it'll automatically be executed by React and will receive the previous state snapshot
+    // setUserInput((prevState) => {
+    //   // returns the value from the previous state snapshot, with the specifically mention value overridden (title in this case)
+    //   return { ...prevState, enteredTitle: event.target.value}
+    // })
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value
+    // });
   };
   return (
     <form>
