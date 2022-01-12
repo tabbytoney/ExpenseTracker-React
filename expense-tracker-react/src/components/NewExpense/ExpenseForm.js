f2d69ec590c0769b693d3301a1d709d5da5861e5
Tabby Douglass <tabbytoney@gmail.com>
2022-01-12 17:39:52 -0600

    Changing to just a single state

--- expense-tracker-react/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker-react/src/components/NewExpense/ExpenseForm.js
@@ -2,22 +2,46 @@ import './ExpenseForm.css';
 import { useState } from 'react';
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+  // using multiple states
+  // const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('');
+
+  // using only one state by passing in an object
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // single state usage
+    // setEnteredTitle(event.target.value);
     // prints the value - aka what's typed - into the title text box
     // console.log(event.target.value)
+
+    setUserInput({
+      // ... pulls out all the key value pairs and returns the values. Title is overridden because it's specifically mentioned here
+      // If you dont do this and just have enteredTitle, enteredAmount and enteredDate just get dropped
+      ...userInput,
+      enteredTitle: event.target.value
+    });
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value
+    });
   };
   return (
     <form>
