761a9d7f971d52413185c38fe4dbe0b7a45f1e25
Tabby Douglass <tabbytoney@gmail.com>
2022-01-25 18:02:52 -0600

    Message shows if there are no expenses for a filtered year

--- expense-tracker-react/src/components/Expenses/Expenses.js
+++ expense-tracker-react/src/components/Expenses/Expenses.js
@@ -13,19 +13,29 @@ const Expenses = (props) => {
   };
 
   // filter returns a new array
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     // returns true if the selected matches the filtered year
     // only items that return true (will be a new array) are stored in filteredExpenses
-    return expense.date.getFullYear().toString() === filteredYear
+    return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No expenses found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+    ));
+  }
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) => (
+        {expensesContent}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found!</p>} */}
+        {/* {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
           <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ))}
+        ))} */}
         {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
