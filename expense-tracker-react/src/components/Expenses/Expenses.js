a052a584b353969dcd05e2a5ecca6bbda78e7de1
Tabby Douglass <tabbytoney@gmail.com>
2022-01-26 16:47:12 -0600

    Reordered Expenses.js, created ExpensesList component

--- expense-tracker-react/src/components/Expenses/Expenses.js
+++ expense-tracker-react/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 // numbers after items mark which item in the array it's providing info for - from App.js
 const Expenses = (props) => {
@@ -19,19 +19,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found!</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-    ));
-  }
+  
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
         {/* {filteredExpenses.length === 0 && <p>No expenses found!</p>} */}
         {/* {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
           <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
